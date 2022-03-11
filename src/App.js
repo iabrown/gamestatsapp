d88d292b80a3a455e8b8827a5128f89491156f4f
Ian Brown <ian@iabdesigns.com>
2022-03-10 23:59:20 -0800

    reverting to previous changes to have ui-react-v1

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 // import '@aws-amplify/ui/dist/styles.css';
-import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react-v1';
 
 
 function App() {
